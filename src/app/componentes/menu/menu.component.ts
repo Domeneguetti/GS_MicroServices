8d684e3a7476c7fa52800c5193c3ab08297ac958
Domeneguetti <domeneguettidaniel@gmail.com>
2024-06-09 15:07:32 -0300

    first commit

---
+++ src/app/componentes/menu/menu.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { menuService } from '../../services/menu.service';
+import { OceanData } from '../../interfaces/oceanData';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-menu',
+  standalone: true,
+  imports: [CommonModule, FormsModule, ReactiveFormsModule],
+  templateUrl: './menu.component.html',
+  styleUrl: './menu.component.css'
+})
+
+export class MenuComponent {
+
+    dados:OceanData[] = [];
+
+    dadosForms: FormGroup = new FormGroup({});
+
+    constructor(private service:menuService, private formBuilder: FormBuilder){
+
+    }
+
+    listar():void{
+      this.service.listarOceanData().subscribe((listaDados) => (this.dados = listaDados));
+    }
+
+    ngOnInit():void{
+      this.listar();
+    }
+}
